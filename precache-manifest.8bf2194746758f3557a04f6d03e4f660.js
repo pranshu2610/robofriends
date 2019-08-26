820d7f282ee26729fd21723abbc9a0fb484bbcb2
pranshu2610 <b218030@iiit-bh.ac.in>
2019-08-27 01:38:41 +0530

    Updates

---
+++ precache-manifest.8bf2194746758f3557a04f6d03e4f660.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2accf8a46464f943eb6f9e08d71aae5",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "7d40970ff7e50f53a9d1",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "07c9aff1dcfbfbbc6fe8",
+    "url": "/robofriends/static/css/main.ec346ad4.chunk.css"
+  },
+  {
+    "revision": "7d40970ff7e50f53a9d1",
+    "url": "/robofriends/static/js/2.b5fdcad1.chunk.js"
+  },
+  {
+    "revision": "07c9aff1dcfbfbbc6fe8",
+    "url": "/robofriends/static/js/main.f546e9ef.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "b868d27513928546767ae242f97f37e4",
+    "url": "/robofriends/static/media/Nexa.b868d275.otf"
+  },
+  {
+    "revision": "4a53759c6a96dc252eacfae2e037fffc",
+    "url": "/robofriends/static/media/bg.4a53759c.jpg"
+  }
+]);
